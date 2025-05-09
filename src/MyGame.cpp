ad9d7ec4d6d80cd66b9cfb69d97e275457906f51
sigma li <sigmalyj@gmail.com>
2025-05-10 00:04:26 +0800

    Frontend Refined

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -2,14 +2,16 @@
 #include "Scenes/StartScene.h"
 #include "Scenes/GameOverScene.h"
 #include "Scenes/BattleScene.h"
+#include "Scenes/HelpScene.h" // 新增 HelpScene 头文件
 
 MyGame::MyGame(QWidget *parent) : QMainWindow(parent)
 {
     // 初始化视图
-    gameOverScene=new GameOverScene(this);
+    gameOverScene = nullptr;
+    battleScene = nullptr;
+    helpScene = nullptr;
+
     view = new QGraphicsView(this);
-    connect(gameOverScene, &GameOverScene::gameOver, this, &MyGame::exitGame);
-    connect(gameOverScene, &GameOverScene::restart, this, &MyGame::restartGame);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setCentralWidget(view);
@@ -17,73 +19,80 @@ MyGame::MyGame(QWidget *parent) : QMainWindow(parent)
     // 创建 StartScene
     startScene = new StartScene(this);
     view->setScene(startScene);
-    view->setFixedSize((int)startScene->width(), (int)startScene->height());
+    view->setFixedSize(1280, 720);
     setFixedSize(view->sizeHint());
 
     // 连接 StartScene 的按钮点击信号
     connect(startScene, &StartScene::startButtonClicked, this, &MyGame::switchToBattleScene);
+    connect(startScene, &StartScene::helpButtonClicked, this, &MyGame::switchToHelpScene); // 新增 Help 按钮信号
 }
 
 void MyGame::switchToBattleScene()
 {
-    // 切换到 BattleScene
-    battleScene = new BattleScene(this);
-    view->setScene(battleScene);
-    view->setFixedSize((int)battleScene->width(), (int)battleScene->height());
-    setFixedSize(view->sizeHint());
-    connect(battleScene, &BattleScene::gameoverSignal, this, &MyGame::switchtogameover);
-    battleScene->startLoop();
+    if (battleScene) {
+        delete battleScene;
+        battleScene = nullptr;
+    }
 
+    battleScene = new BattleScene(this);
+    connect(battleScene, &BattleScene::gameoverSignal, this, &MyGame::switchtogameover);
+
+    view->setScene(battleScene);
+    view->setFixedSize(1280, 720);
+    setFixedSize(view->sizeHint());
+    battleScene->startLoop();
+}
+
+void MyGame::switchToHelpScene()
+{
+    if (helpScene) {
+        delete helpScene;
+        helpScene = nullptr;
+    }
+
+    helpScene = new HelpScene(this);
+    connect(helpScene, &HelpScene::returnButtonClicked, this, &MyGame::switchToStartScene); // 连接 Return 按钮信号
+
+    view->setScene(helpScene);
+    view->setFixedSize(1280, 720);
+    setFixedSize(view->sizeHint());
+}
+
+void MyGame::switchToStartScene()
+{
+    view->setScene(startScene);
+    view->setFixedSize(1280, 720);
+    setFixedSize(view->sizeHint());
 }
 
 void MyGame::switchtogameover(QString text)
 {
-    if(gameOverScene)
-    {
+    if (gameOverScene) {
         delete gameOverScene;
+        gameOverScene = nullptr;
     }
+
     gameOverScene = new GameOverScene(this);
     connect(gameOverScene, &GameOverScene::gameOver, this, &MyGame::exitGame);
     connect(gameOverScene, &GameOverScene::restart, this, &MyGame::restartGame);
+
     gameOverScene->setSceneRect(0, 0, 1280, 720);
     view->setScene(gameOverScene);
     gameOverScene->showGameOverMessage(text);
-    view->setFixedSize((int) view->scene()->width(), (int) view->scene()->height());
+    view->setFixedSize(1280, 720);
     setFixedSize(view->sizeHint());
-    battleScene->timer->stop();
-}
 
-void MyGame::switchtobattle()
-{
-    if(battleScene)
-    {
-        delete battleScene;
+    if (battleScene && battleScene->timer) {
+        battleScene->timer->stop();
     }
-    // if(gameOverScene)
-    // {
-    //     delete gameOverScene;
-    // }
-    // gameOverScene = new GameOverScene(this);
-    connect(gameOverScene, &GameOverScene::gameOver, this, &MyGame::exitGame);
-    connect(gameOverScene, &GameOverScene::restart, this, &MyGame::restartGame);
-    battleScene = new BattleScene(this);
-    connect(battleScene, &BattleScene::gameoverSignal, this, &MyGame::switchtogameover);
-
-    view->setScene(battleScene);
-    view->setFixedSize((int) view->scene()->width(), (int) view->scene()->height());
-    setFixedSize(view->sizeHint());
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setCentralWidget(view);
-    battleScene->startLoop();
 }
 
 void MyGame::exitGame()
 {
     QApplication::quit();
 }
+
 void MyGame::restartGame()
 {
-    switchtobattle();
+    switchToBattleScene();
 }
-
